2fbf325dc2f694dc2c1c3526629948ab78fde854
Geod24 <pro.mathias.lang@gmail.com>
2021-08-03 17:11:18 +0900

    Add a simple test

---
+++ source/app.d
@@ -0,0 +1,14 @@
+import core.stdc.stdio, core.stdc.stdlib;
+import std.stdio;
+
+void main()
+{
+	writeln("Edit source/app.d to start your project.");
+}
+
+unittest
+{
+    printf("Hello World!\n");
+    printf("Please flush my output\n");
+    abort();
+}
